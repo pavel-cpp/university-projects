fb5aa60044fd3c34a6130ba1ba9caee036a5e7e3
ToruItz <93165535+ToruItz@users.noreply.github.com>
2021-10-27 13:43:46 +0300

    Add files via upload

--- pz04/main.c
+++ pz04/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	
+	FILE *file;
+	file = fopen("/Users/user/pz04/firstfile.txt", "w");
+	for(i = 0, i <= 100, i++){
+		if(i % 10 = 0)
+			fprintf(file, "\n");
+		fprintf(file, "123");
+	}
+	fclose(file);
+	return 0;
 }
