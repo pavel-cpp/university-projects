c6835b69864befd83bc27cfef41014f388b0fd0b
ToruItz <93165535+ToruItz@users.noreply.github.com>
2021-12-07 21:55:41 +0300

    Add files via upload

---
+++ lr05/p01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MyFunction.h"
+
+int main(){
+    system("clear");
+	int cases;
+	printf("Задача №03 Ремденок Павел Александрович, ИКПИ-15\nВыберите ввод исходных данных: \n 1) с экрана; \n 2) из файла; \n 3) рандомные значения; \n 4) Из тела программы; \n Ввод: ");
+	scanf("%d", &cases);
+	switch(cases) {
+        case 27:
+            return 0;
+        case 1:
+            first(cases);
+            break;
+        case 2:
+            second(cases);
+            break;
+        case 3:
+            third(cases);
+            break;
+        case 4:
+            fourth(cases);
+            break;
+    }
+    return 0;
+}
