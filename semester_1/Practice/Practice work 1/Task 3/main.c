8457cf922ee93d476d37a69a1905cf59c5454270
Pavel Remdenok <toruty174@gmail.com>
2023-08-30 21:10:06 +0300

    Task 2 from PW 1

---
+++ semester_1/Practice/Practice work 1/Task 3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int x;
+    printf("Введите x: ");
+    scanf("%d", &x);
+    if(x > 0){
+        printf("y = %d", 2 * x - 10);
+    }else if(x == 0){
+        printf("y = 0");
+    }else{
+        printf("y = %d", 2 * abs(x) - 1);
+    }
+    return 0;
+}
